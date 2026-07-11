9b774aad84f43ccde9e5249343bb980f0e1528ff
Jay <jmartblues@ukr.net>
2026-07-11 16:29:22 +0300

    Add sample weather data entries

---
+++ sample_data.rb
@@ -0,0 +1,29 @@
+require_relative "weather_entry"
+
+ENTRIES = [
+
+  WeatherEntry.new(
+    "2026-07-01",
+    "Sunny",
+    27
+  ),
+
+  WeatherEntry.new(
+    "2026-07-02",
+    "Rain",
+    20
+  ),
+
+  WeatherEntry.new(
+    "2026-07-03",
+    "Sunny",
+    31
+  ),
+
+  WeatherEntry.new(
+    "2026-07-04",
+    "Cloudy",
+    24
+  )
+
+]
